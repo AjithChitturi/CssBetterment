fd04d131a2300b11b68ff175ab8bbf33b2d656e9
sivakumarssk <sivakumarslks@gmail.com>
2025-02-17 03:43:26 +0530

    changes done by siva

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './CssBetterment/CSS/templates/**/*.html', // Adjust this path to your HTML templates
+    './CssBetterment/static/js/**/*.js',         // Adjust this path to your JS files (if any)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
